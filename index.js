f9f8332f3e194090fca4a06d5161490c2d1e3e1d
Mike Bannister <notimpossiblemike@gmail.com>
2016-12-23 19:41:44 -0800

    Fix initial build from happening out of band

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const installApp = () => {
 }
 const buildApp = () => {
   console.info(' - build app')
-  runProcess('yarn run build', { cwd: workDir })
+  return runProcess('yarn run build', { cwd: workDir })
 }
 
 let isRestarting = false
