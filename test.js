d5b7c380515ecf9dbf13530468ed8d151e2a9a7c
Mike Bannister <notimpossiblemike@gmail.com>
2016-12-23 17:09:45 -0800

    Just fiddling to make the tests read better

--- test.js
+++ test.js
@@ -4,51 +4,54 @@ const fs = require('fs-promise')
 const request = require('request')
 const terminate = require('terminate')
 
-const runCommand = () => {
+const TIMEOUT = 2000
+const PORT = 55555
+const URL = `http://localhost:${PORT}`
+
+const cleanupSync = () => {
   fs.removeSync('/tmp/doof')
   fs.removeSync('/tmp/moof')
   fs.copySync('./example-app', '/tmp/doof')
-  const running = spawn('./bin/node-runner', ['/tmp/doof', '/tmp/moof'])
-
-  // TODO this is useful to uncomment sometimes
-  // running.childprocess.stdout.pipe(process.stdout)
-  // running.childprocess.stderr.pipe(process.stderr)
-
-  return running
 }
 
-const replaceInAppIndex = (findString, replaceString) => {
+const runCommand = () => spawn('./bin/node-runner', [
+  '/tmp/doof',
+  '/tmp/moof'
+])
+
+const replaceStringInAppIndexJs = (findString, replaceString) => {
   const contentPath = '/tmp/doof/index.js'
   const content = fs.readFileSync(contentPath).toString()
   fs.writeFileSync(contentPath, content.replace(findString, replaceString))
 }
 
-const TIMEOUT = 2000
-const PORT = 55555
-const URL = `http://localhost:${PORT}`
-
 // NOTE testing is rather shallow
 
-const waitForBodyToContain = (childProcess, expectedBody, callback) => {
-  setTimeout(() => {
-    request(URL, (error, res) => {
-      if (error) return done(error)
-
-      assert.equal(res.body, expectedBody)
-      terminate(childProcess.pid, callback)
-    })
-  }, TIMEOUT)
-}
-
 describe('node runner', () => {
+  let runningCommand
+
+  beforeEach(() => {
+    cleanupSync()
+    runningCommand = runCommand()
+  })
+
+  const expectAppEndpointToContain = (expectedBody, callback) => {
+    setTimeout(() => {
+      request(URL, (error, res) => {
+        if (error) return done(error)
+
+        assert.equal(res.body, expectedBody)
+        terminate(runningCommand.pid, callback)
+      })
+    }, TIMEOUT)
+  }
+
   it('runs app', done => {
-    const running = runCommand()
-    waitForBodyToContain(running, '`default` example response', done)
+    expectAppEndpointToContain('`default` example response', done)
   })
 
   it('restarts app when a file changes', done => {
-    const running = runCommand()
-    replaceInAppIndex('default', '!!!changed!!!')
-    waitForBodyToContain(running, '`!!!changed!!!` example response', done)
+    replaceStringInAppIndexJs('default', '!!!changed!!!')
+    expectAppEndpointToContain('`!!!changed!!!` example response', done)
   })
 })
